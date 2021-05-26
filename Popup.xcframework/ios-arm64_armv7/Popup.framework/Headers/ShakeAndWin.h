e6378b332aff25c75459015f22c631d7d0fcfb73
Shiv <sky.shivy@gmail.com>
2021-05-26 14:34:51 +0530

    sec

---
+++ Popup.xcframework/ios-arm64_armv7/Popup.framework/Headers/ShakeAndWin.h
@@ -0,0 +1,18 @@
+//
+//  ShakeAndWin.h
+//  ShakeAndWin
+//
+//  Created by SKY on 29/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShakeAndWin.
+FOUNDATION_EXPORT double ShakeAndWinVersionNumber;
+
+//! Project version string for ShakeAndWin.
+FOUNDATION_EXPORT const unsigned char ShakeAndWinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShakeAndWin/PublicHeader.h>
+
+
