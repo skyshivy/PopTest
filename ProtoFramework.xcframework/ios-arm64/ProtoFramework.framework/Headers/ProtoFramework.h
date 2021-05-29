417d97de7f4f1868b227672d258647d63dfaa72e
Shiv <sky.shivy@gmail.com>
2021-05-29 09:52:36 +0530

    new

---
+++ ProtoFramework.xcframework/ios-arm64/ProtoFramework.framework/Headers/ProtoFramework.h
@@ -0,0 +1,18 @@
+//
+//  ProtoFramework.h
+//  ProtoFramework
+//
+//  Created by SKY on 29/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProtoFramework.
+FOUNDATION_EXPORT double ProtoFrameworkVersionNumber;
+
+//! Project version string for ProtoFramework.
+FOUNDATION_EXPORT const unsigned char ProtoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProtoFramework/PublicHeader.h>
+
+
